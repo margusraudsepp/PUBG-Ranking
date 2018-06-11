dd8ab58947cfb8d8cfb3efc27dd222e432284bfa
Margus Raudsepp <mcklapp@hotmail.com>
2018-06-11 20:04:28 +0300

    Update addUser.ts

--- src/cmd/server_cmds/addUser.ts
+++ src/cmd/server_cmds/addUser.ts
@@ -51,9 +51,9 @@ export class AddUser extends Command {
                     if (pubgId && pubgId !== '') {
                         let registered: boolean = await sqlServerRegisteryService.registerUserToServer(pubgId, message.guild.id);
                         if(registered) {
-                            message.edit(`Added 1 ${username}`);
+                            message.edit(`Added ${username}`);
                         } else {
-                            message.edit(`Could 2 not add ${username}`);
+                            message.edit(`Could not add ${username}`);
                         }
                     } else {
                         message.edit(`Could not find ${username} on the ${region} region. Double check the username and region.`);
