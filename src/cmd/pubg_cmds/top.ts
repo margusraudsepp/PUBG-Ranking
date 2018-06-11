c5b77824c21a1935efcd12301b8c6e1593488017
Margus Raudsepp <mcklapp@hotmail.com>
2018-06-11 21:57:27 +0300

    Update top.ts

--- src/cmd/pubg_cmds/top.ts
+++ src/cmd/pubg_cmds/top.ts
@@ -103,7 +103,7 @@ export class Top extends Command {
                 playersInfo.sort(function (a: Player, b: Player) { return (+b.rating) - (+a.rating); });
                 let topPlayers: Player[] = playersInfo.slice(0, registeredPlayers.length);
                 let embed: Discord.RichEmbed = new Discord.RichEmbed()
-                    .setTitle('Top ' + amount + ' local players')
+                    .setTitle('Top ' + amount + ' local players' + Discord.Client)
                     .setDescription('Season:\t' + SeasonEnum[season] + '\nRegion:\t' + region.toUpperCase() + '\nMode: \t' + mode.toUpperCase() + '\nSquad Size: \t' + SquadSizeEnum[squadSize])
                     .setColor(0x00AE86)
                     .setFooter('Data retrieved from https://pubg.op.gg/')
@@ -120,8 +120,8 @@ export class Top extends Command {
                     ratings += ratingStr + '\n';
                     kds += kdsStr + '\n';
                 }
-            embed.addField('asd', Discord.Client, true)
-                embed.addField('Name', names, true)
+                embed.addField('No', i, true)
+                    .addField('Name', names, true)
                     .addField('Rank / Rating', ratings, true)
                     .addField('KD / KDA / Avg Dmg', kds, true);
                 await msg.edit({ embed });
